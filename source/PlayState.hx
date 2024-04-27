85fbd19168cb257ae2774b89f1f64775df494135
mahikotahi <portilizenseason@gmail.com>
2024-04-27 08:13:53 -0600

    baldoints

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,5 +1,6 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.group.FlxGroup.FlxTypedGroup;
@@ -8,19 +9,37 @@ class PlayState extends FlxState
 {
 	var items:Array<Dynamic> = ['Apple', 'Gem', 'Hammer', 'Overrider', 'RedCan', 'Stabber'];
 	var itemGRP:FlxTypedGroup<FlxSprite>;
+	var baldiboints:Int = 0;
 
 	override public function create()
 	{
 		itemGRP = new FlxTypedGroup<FlxSprite>();
-		add(itemGRP);
 
-		for (i in 0...items.length) {}
+		for (i in 0...items.length)
+		{
+			var sprite:FlxSprite = new FlxSprite(0, 0);
+			sprite.setPosition(FlxG.random.float(320, 960), FlxG.random.float(180, 360));
+			sprite.loadGraphic('assets/images/' + items[FlxG.random.int(0, 6)] + '.png');
+			itemGRP.add(sprite);
+		}
+
+		add(itemGRP);
 
 		super.create();
 	}
 
 	override public function update(elapsed:Float)
 	{
+		for (i in 0...itemGRP.members.length)
+		{
+			if (FlxG.mouse.overlaps(itemGRP.members[i]) && FlxG.mouse.released)
+			{
+				itemGRP.members[i].setPosition(FlxG.random.float(320, 960), FlxG.random.float(180, 360));
+				itemGRP.members[i].loadGraphic('assets/images/' + items[FlxG.random.int(0, 6)] + '.png');
+				baldiboints += FlxG.random.int(10, 200);
+			}
+		}
+
 		super.update(elapsed);
 	}
 }
