5c856f3884521368371b01cd44d7ca1409af41e6
mahikotahi <portilizenseason@gmail.com>
2024-04-27 08:02:26 -0600

    group up children

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2,33 +2,19 @@ package;
 
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.group.FlxGroup.FlxTypedGroup;
 
 class PlayState extends FlxState
 {
-	var apple:FlxSprite;
-	var hammer:FlxSprite;
-	var holder:FlxSprite;
-	var gem:FlxSprite;
-	var redcan:FlxSprite;
-	var stabber:FlxSprite;
-	var overrider:FlxSprite;
+	var items:Array<Dynamic> = ['Apple', 'Gem', 'Hammer', 'Overrider', 'RedCan', 'Stabber'];
+	var itemGRP:FlxTypedGroup<FlxSprite>;
 
 	override public function create()
 	{
-		apple = new FlxSprite(0, 0).loadGraphic('assets/images/Apple.png');
-		hammer = new FlxSprite(0, 0).loadGraphic('assets/images/Hammer.png');
-		holder = new FlxSprite(0, 0).loadGraphic('assets/images/Holder.png');
-		gem = new FlxSprite(0, 0).loadGraphic('assets/images/Gem.png');
-		redcan = new FlxSprite(0, 0).loadGraphic('assets/images/RedCan.png');
-		stabber = new FlxSprite(0, 0).loadGraphic('assets/images/Stabber.png');
-		overrider = new FlxSprite(0, 0).loadGraphic('assets/images/Overrider.png');
-		add(apple);
-		add(hammer);
-		add(holder);
-		add(gem);
-		add(redcan);
-		add(stabber);
-		add(overrider);
+		itemGRP = new FlxTypedGroup<FlxSprite>();
+		add(itemGRP);
+
+		for (i in 0...items.length) {}
 
 		super.create();
 	}
