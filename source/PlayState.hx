54410e712a67be35a924cb5f636d9f7625953604
mahikotahi <portilizenseason@gmail.com>
2024-04-27 08:19:41 -0600

    positions

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -18,7 +18,7 @@ class PlayState extends FlxState
 		for (i in 0...items.length)
 		{
 			var sprite:FlxSprite = new FlxSprite(0, 0);
-			sprite.setPosition(FlxG.random.float(0, 960), FlxG.random.float(180, 360));
+			sprite.setPosition(FlxG.random.float(0, 960), FlxG.random.float(0, 530));
 			sprite.loadGraphic('assets/images/' + items[FlxG.random.int(0, 6)] + '.png');
 			itemGRP.add(sprite);
 		}
@@ -34,7 +34,7 @@ class PlayState extends FlxState
 		{
 			if (FlxG.mouse.overlaps(itemGRP.members[i]) && FlxG.mouse.justPressed)
 			{
-				itemGRP.members[i].setPosition(FlxG.random.float(0, 960), FlxG.random.float(180, 360));
+				itemGRP.members[i].setPosition(FlxG.random.float(0, 960), FlxG.random.float(0, 530));
 				itemGRP.members[i].loadGraphic('assets/images/' + items[FlxG.random.int(0, 6)] + '.png');
 				baldiboints += FlxG.random.int(10, 200);
 			}
